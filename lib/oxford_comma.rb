a62fea5bb1e3f77645c085cf165ad98197afc1f6
Jesse McReady <jessemcready@gmail.com>
2018-06-21 01:30:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,12 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   else
+    # get last element of the array
     last_element = array[-1]
+    # create the sentence, adding a comma after each element
     oxford_sentence = array.join(", ")
+    # insert "and" before the last element
     oxford_sentence.insert(-last_element.size - 1, "and ")
-    oxford_sentence
+    #oxford_sentence
   end
 end
